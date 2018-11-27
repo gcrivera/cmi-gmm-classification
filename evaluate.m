ec64e590b6f7b0ed683ef5c06eb6e25062f64c1a
Gabrielle Rivera <gcrivera@mit.edu>
2018-11-27 14:19:41 -0500

    evaluation done, just needs testing

--- evaluate.m
+++ evaluate.m
@@ -10,8 +10,11 @@ cmi4_data = readNPY('data/test_cmi4_13f.npy');
 cmi5_data = readNPY('data/test_cmi5_13f.npy');
 all_data = [ cmi1_data cmi2_data cmi3_data cmi4_data cmi5_data ];
 
-% Set up args
 models = {load(strcat(model_path, 'cmi1.mat')), load(strcat(model_path, 'cmi2.mat')), load(strcat(model_path, 'cmi3.mat')), load(strcat(model_path, 'cmi4.mat')), load(strcat(model_path, 'cmi5.mat'))};
+
+true_labels = [];
+predicted_labels = [];
+matching = 0;
 
 for i = 1:5
     data = all_data(i);
@@ -33,13 +36,18 @@ for i = 1:5
     % scores((utterance_num-1)*5 + model #) gives utterance score for model
     scores = score_gmm_trials(models, tests, trials, strcat(model_path, 'ubm.mat'));
     
+    for j = 1:data_size(1)
+        partial_idx = (j-1)*5;
+        utterance_scores = scores(1+partial_idx:5+partial_idx, 1);
+        [val, idx] = max(utterance_scores);
+        true_labels(end+1) = i;
+        predicted_labels(end+1) = idx;
+        if i == idx
+            matching = matching + 1;
+        end
+    end
 end
 
-
-% View confusion matrix
-% Not sure what scores outputs
-% title('CMI Likelihood (GMM Model)');
-% ylabel('Test # (True CMI)'); xlabel('Model #');
-% colorbar; drawnow; axis xy
-% figure
-% eer = compute_eer(scores, answers, false);
+disp('Accuracy:')
+disp(double(matching) / double(size(true_labels)))
+cm = confusionchart(true_labels, predicted_labels, 'RowSummary', 'row-normalized', 'ColumnSummary', 'column-normalized');

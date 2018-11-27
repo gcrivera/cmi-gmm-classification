c745b2e524bf6eec9847815e1f15982f08e860f3
Gabrielle Rivera <gcrivera@mit.edu>
2018-11-27 13:01:39 -0500

    working on evaluation portion

--- evaluate.m
+++ evaluate.m
@@ -8,19 +8,33 @@ cmi2_data = readNPY('data/test_cmi2_13f.npy');
 cmi3_data = readNPY('data/test_cmi3_13f.npy');
 cmi4_data = readNPY('data/test_cmi4_13f.npy');
 cmi5_data = readNPY('data/test_cmi5_13f.npy');
+all_data = [ cmi1_data cmi2_data cmi3_data cmi4_data cmi5_data ];
 
 % Set up args
 models = {load(strcat(model_path, 'cmi1.mat')), load(strcat(model_path, 'cmi2.mat')), load(strcat(model_path, 'cmi3.mat')), load(strcat(model_path, 'cmi4.mat')), load(strcat(model_path, 'cmi5.mat'))};
-tests = {cmi1_data, cmi2_data, cmi3_data, cmi4_data, cmi5_data};
-trials = [  1 1; 1 2; 1 3; 1 4; 1 5;
-            2 1; 2 2; 2 3; 2 4; 2 5;
-            3 1; 3 2; 3 3; 3 4; 3 5;
-            4 1; 4 2; 4 3; 4 4; 4 5;
-            5 1; 5 2; 5 3; 5 4; 5 5  ];
 
-% Generate scores
+for i = 1:5
+    data = all_data(i);
+    data_size = size(data);
+    tests = {};
+    for j = 1:data_size(1)
+        utterance_data = data(j);
+        utterance_data = utterance_data(:,any(data,1));
+        tests{end+1} = utterance_data;
+    end
+    
+    trials = zeros(5*data_size(1), 2);
+    for j = 1:data_size(1)
+        trials((j-1)*5+1:j*5,1) = 1:5;
+        trials((j-1)*5+1:j*5,2) = j;
+    end
+    
+    % should be same size as num rows of trials
+    % scores((utterance_num-1)*5 + model #) gives utterance score for model
+    scores = score_gmm_trials(models, tests, trials, strcat(model_path, 'ubm.mat'));
+    
+end
 
-scores = score_gmm_trials(models, tests, trials, strcat(model_path, 'ubm.mat'));
 
 % View confusion matrix
 % Not sure what scores outputs

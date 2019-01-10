2b015bf8987198ce1a27cd5ec6b55e0ff91774c3
Gabrielle Rivera <gcrivera@mit.edu>
2019-01-10 18:00:56 -0500

    ready for mfcc 1024 generation

--- generate_gmms.m
+++ generate_gmms.m
@@ -1,15 +1,15 @@
 addpath(genpath('matlab-reqs'))
 
-nmix = 128;
-model_path = 'models/mfcc_spec/128c/';
+nmix = 1024;
+model_path = 'models/mfcc/1024c/';
 
 % Load training data
 disp('Loading data...')
-cmi1_data = double(readNPY('data/mfcc_spec/train_cmi1_20f.npy')');
-cmi2_data = double(readNPY('data/mfcc_spec/train_cmi2_20f.npy')');
-cmi3_data = double(readNPY('data/mfcc_spec/train_cmi3_20f.npy')');
-cmi4_data = double(readNPY('data/mfcc_spec/train_cmi4_20f.npy')');
-cmi5_data = double(readNPY('data/mfcc_spec/train_cmi5_20f.npy')');
+cmi1_data = double(readNPY('data/mfcc/t2_train_cmi1_20f.npy')');
+cmi2_data = double(readNPY('data/mfcc/t2_train_cmi2_20f.npy')');
+cmi3_data = double(readNPY('data/mfcc/t2_train_cmi3_20f.npy')');
+cmi4_data = double(readNPY('data/mfcc/t2_train_cmi4_20f.npy')');
+cmi5_data = double(readNPY('data/mfcc/t2_train_cmi5_20f.npy')');
 
 % all_data = num2cell(cat(1, cmi1_data, cmi2_data, cmi3_data, cmi4_data, cmi5_data)', 1);
 all_data = {cmi1_data; cmi2_data; cmi3_data; cmi4_data; cmi5_data};
